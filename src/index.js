e90a3b3b66261755ac313d3d625a1d3dc53c0f20
subrat kmr <35666367+subrat-kmr@users.noreply.github.com>
2021-02-05 10:52:32 +0530

    initial commit first commit

--- src/index.js
+++ src/index.js
@@ -1,46 +1,44 @@
-const http = require('http');
+const http = require("http");
 
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 async function main() {
-    // const {MongoClient} = require('mongodb');
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://subratkmr:subratkmr@firstcluster.vykj4.mongodb.net/firdtDB?retryWrites=true&w=majority";
+  // const {MongoClient} = require('mongodb');
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri =
+    "mongodb+srv://subratkmr:subratkmr@firstcluster.vykj4.mongodb.net/firdtDB?retryWrites=true&w=majority";
 
-
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-        // Make the appropriate DB calls
-        await listDatabases(client);
-    } catch (e) {
-
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 main().catch(console.error);
 
 async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
-
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!\n");
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
